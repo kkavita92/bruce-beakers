fe0894880271f6dcef1dfeac49558c7c74db0cc0
kkavita92 <kkavita92@gmail.com>
2017-08-13 08:17:49 +0800

    Implement filling and emptying beakers on front end

--- src/Interface.js
+++ src/Interface.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
 
   var puzzle = new Puzzle();
+  var fiveLitreBeaker = puzzle.fiveLitreBeaker;
+  var threeLitreBeaker = puzzle.threeLitreBeaker;
 
-  $('#fivelitrebeaker').text(puzzle.fiveLitreBeaker.currentCapacity);
-  $('#threelitrebeaker').text(puzzle.threeLitreBeaker.currentCapacity);
+  $('#fivelitrebeaker').text(fiveLitreBeaker.currentCapacity);
+  $('#threelitrebeaker').text(threeLitreBeaker.currentCapacity);
+
+  $('#fillfive').click(function() {
+    puzzle.fillup(fiveLitreBeaker);
+    $('#fivelitrebeaker').text(fiveLitreBeaker.currentCapacity);
+    return false;
+  });
+
+  $('#fillthree').click(function() {
+    puzzle.fillup(threeLitreBeaker);
+    $('#threelitrebeaker').text(threeLitreBeaker.currentCapacity);
+    return false;
+  });
+
+  $('#emptyfive').click(function() {
+    puzzle.empty(fiveLitreBeaker);
+    $('#fivelitrebeaker').text(fiveLitreBeaker.currentCapacity);
+    return false;
+  });
+
+  $('#emptythree').click(function() {
+    puzzle.empty(threeLitreBeaker);
+    $('#threelitrebeaker').text(threeLitreBeaker.currentCapacity);
+    return false;
+  });
 
 
 });
